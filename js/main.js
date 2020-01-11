0a7b06e182a3430005c51b1ee2187573c69c59fd
WelderMileu <welderfelix21@gmail.com>
2020-01-11 12:57:00 -0200

    deixando o menu de forma responsiva

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 (function(){
-	//
+	'use static';	
+	const $ = event => document.querySelector(event);
+
+
+	// menu responsivo
+
+	$('.bars').addEventListener('click', function() {
+		$('.nav-response').style.left = "10%";
+		$('.nav-response').style.opacity = "1";
+		$('.nav-response').style.visibility = "visible";
+	});
+
 })();
