4cbf16e71b16b5e5dd643b9c3de77ea33a111bae
WelderMileu <welderfelix21@gmail.com>
2020-01-11 17:18:19 -0200

    ajustando o menu responsivo

--- js/main.js
+++ js/main.js
@@ -6,9 +6,11 @@
 	// menu responsivo
 
 	$('.bars').addEventListener('click', function() {
-		$('.nav-response').style.left = "10%";
-		$('.nav-response').style.opacity = "1";
-		$('.nav-response').style.visibility = "visible";
+		$('.nav-response').classList.toggle('abrir');
+	});
+
+	$('#close').addEventListener('click', function() {
+		$('.nav-response').addClass('close');
 	});
 
 })();
